f1b94b672b50a14a4dcc2fd51f3c673a03be183e
sinta3 <sinta.tri38@gmail.com>
2020-07-30 06:45:13 +0700

    add crud

--- routes/user/controller.js
+++ routes/user/controller.js
@@ -1,5 +1,8 @@
 const { User } = require('../../models');
 const { hashPassword } = require('../../helpers');
+const bcrypt = require('bcryptjs');
+const { createToken } = require('../../helpers/token');
+const { findByIdAndDelete } = require('../../models/User');
 
 module.exports = {
     Registration: async (req, res) => {
@@ -16,16 +19,149 @@ module.exports = {
                 });
             }
 
-          req.body.password = await hashPassword(req.body.password);
+            req.body.password = await hashPassword(req.body.password);
 
             const result = await User.create({
-                ...req.body
+                ...req.body,
             });
             res.send({ message: 'success', data: result });
         } catch (error) {
             res.send(error);
         }
     },
-    
-    
+
+    userLogin: async (req, res) => {
+        const { email, password } = req.body;
+
+        try {
+            const registeredUser = await User.findOne({
+                email,
+            });
+
+            if (registeredUser) {
+                bcrypt
+                    .compare(password, registeredUser.password)
+                    .then((result) => {
+                        if (result === true) {
+                            const userData = {
+                                id: registeredUser._id,
+                                name: registeredUser.name,
+                                email: registeredUser.email,
+                                role: registeredUser.role,
+                            };
+
+                            const token = createToken(userData);
+                            res.send({
+                                message: `Login Succesfull`,
+                                token,
+                                userData,
+                            });
+                        } else {
+                            return res.send(`Your email or password is wrong`);
+                        }
+                    });
+            } else {
+                res.send(`Your email is not registered`);
+            }
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    updateProfil: async (req, res) => {
+        const id = req.params.id;
+
+        try {
+            const result = await User.findByIdAndUpdate(
+                { _id: id },
+                {
+                    ...req.body,
+                }
+            );
+
+            res.send({ message: 'success add profil', data: result });
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    getAllUser: async (req, res) => {
+        try {
+            const result = await User.find();
+
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    getAllAudience: async (req, res) => {
+        try {
+            const result = await User.find({ role: 'audience' });
+
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    getAllSpeaker: async (req, res) => {
+        try {
+            const result = await User.find({ role: 'speaker' });
+
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    filterByCategory: async (req, res) => {
+        const category = req.query.category;
+        try {
+            const result = await User.find({
+                category: {
+                    $regex: category,
+                    $options: 'i',
+                },
+            });
+
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+            console.log(error);
+        }
+    },
+    getUser: async (req, res) => {
+        const id = req.params.id;
+        try {
+            const result = await User.findById(id);
+
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+            console.log(error);
+        }
+    },
+    filterByName: async (req, res) => {
+        const user = req.query.user;
+        console.log(user);
+        try {
+            const result = await User.find({
+                name: 'sinta',
+                // name: {
+                //     $regex: user,
+                //     $options: 'i',
+                // },
+            });
+
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    deleteUser: async (req, res) => {
+        const id = req.params.id;
+        try {
+            await User.findByIdAndDelete(id);
+
+            res.send({ message: 'deleted' });
+        } catch (error) {
+            res.send(error);
+        }
+    },
 };
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { Registration } = require('./controller');
+const {
+    Registration,
+    userLogin,
+    updateProfil,
+    getAllUser,
+    getAllAudience,
+    getAllSpeaker,
+    getUser,
+    filterByCategory,
+    filterByName,
+    deleteUser,
+} = require('./controller');
 
 router.post('/', Registration);
+router.post('/login', userLogin);
+router.put('/:id', updateProfil);
+router.get('/allUser', getAllUser);
+router.get('/audience', getAllAudience);
+router.get('/speaker', getAllSpeaker);
+router.get('/:id', getUser);
+router.get('/category/', filterByCategory);
+router.get('/name/', filterByName);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
