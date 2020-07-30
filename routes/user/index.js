4da603443af4bbd9b274244a6c28859893f1166e
sinta3 <sinta.tri38@gmail.com>
2020-07-30 22:02:17 +0700

    add search in user

--- routes/user/index.js
+++ routes/user/index.js
@@ -13,6 +13,9 @@ const {
     filterByCategory,
     filterByName,
     deleteUser,
+    filterByLocation,
+    getActiveSpeaker,
+    getInActiveSpeaker,
 } = require('./controller');
 
 router.post('/', Registration);
@@ -21,9 +24,12 @@ router.put('/:id', verifyToken, updateProfil);
 router.get('/allUser', verifyToken, getAllUser);
 router.get('/audience', verifyToken, getAllAudience);
 router.get('/speaker', verifyToken, getAllSpeaker);
-router.get('/:id', verifyToken, getUser);
+router.get('/user/:id', verifyToken, getUser);
 router.get('/category/', verifyToken, filterByCategory);
 router.get('/name/', verifyToken, filterByName);
+router.get('/location/', verifyToken, filterByLocation);
+router.get('/activeSpeaker', verifyToken, getActiveSpeaker);
+router.get('/InActiveSpeaker', verifyToken, getInActiveSpeaker);
 router.delete('/:id', verifyToken, deleteUser);
 
 module.exports = router;
