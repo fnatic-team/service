56de2ea4d36d1bdc492c0ac0c76da598eeb23875
Rohmad <rohmad37@ymail.com>
2020-08-07 14:57:47 +0700

    sosial login service

--- config/index.js
+++ config/index.js
@@ -3,6 +3,10 @@ const {
     MONGODB_URI_LIVE,
     MONGODB_URI_LOCAL,
     JWT_SECRET,
+    GOOGLE_API_KEY,
+    GOOGLE_API_SECRET,
+    FACEBOOK_API_KEY,
+    FACEBOOK_API_SECRET,
 } = require('./environment');
 const db = require('./database');
 
@@ -11,5 +15,9 @@ module.exports = {
     MONGODB_URI_LIVE,
     MONGODB_URI_LOCAL,
     JWT_SECRET,
+    GOOGLE_API_KEY,
+    GOOGLE_API_SECRET,
+    FACEBOOK_API_KEY,
+    FACEBOOK_API_SECRET,
     db,
 };
--- helpers/index.js
+++ helpers/index.js
@@ -1,8 +1,18 @@
 const { hashPassword } = require('./bcrypt');
 const { createToken, verifyToken } = require('./token');
+const {
+    facebookAuthenticate,
+    googleAuthenticate,
+    jwtAuthenticate,
+} = require('./auth');
+const strategies = require('./strategies');
 
 module.exports = {
     createToken,
     verifyToken,
     hashPassword,
+    facebookAuthenticate,
+    googleAuthenticate,
+    strategies,
+    jwtAuthenticate,
 };
